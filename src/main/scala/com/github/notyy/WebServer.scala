c0e674f50c0e22c7c117d2b123905faccad7e305
notyy <notyycn@gmail.com>
2016-11-26 15:08:24 +0800

    a basic runnable webserver

---
+++ src/main/scala/com/github/notyy/WebServer.scala
@@ -0,0 +1,51 @@
+package com.github.notyy
+
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.{ContentTypes, HttpEntity}
+import akka.http.scaladsl.server.Directives._
+import akka.stream.ActorMaterializer
+import akka.stream.scaladsl._
+import akka.util.ByteString
+
+import scala.io.StdIn
+import scala.util.Random
+
+object WebServer {
+  def main(args: Array[String]) {
+
+    implicit val system = ActorSystem("my-system")
+    implicit val materializer = ActorMaterializer()
+    // needed for the future flatMap/onComplete in the end
+    implicit val executionContext = system.dispatcher
+
+    val numbers = Source.fromIterator(() =>
+      Iterator.continually(Random.nextInt()))
+
+    val route =
+      path("hello") {
+        get {
+          complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Say hello to akka-http</h1>"))
+        }
+      } ~
+        path("random") {
+          get {
+            complete(
+              HttpEntity(
+                ContentTypes.`text/plain(UTF-8)`,
+                // transform each number to a chunk of bytes
+                numbers.map(n => ByteString(s"$n\n"))
+              )
+            )
+          }
+        }
+
+    val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
+
+    println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
+    StdIn.readLine() // let it run until user presses return
+    bindingFuture
+      .flatMap(_.unbind()) // trigger unbinding from the port
+      .onComplete(_ => system.terminate()) // and shutdown when done
+  }
+}
