5c3fb862dc99ea6c06b135f56ad98c3a12a024a1
notyy <notyycn@gmail.com>
2016-12-20 14:34:36 +0800

    must bind to 0.0.0.0 instead of localhost, so that the docker container can be accessed

--- src/main/scala/com/github/notyy/WebServer.scala
+++ src/main/scala/com/github/notyy/WebServer.scala
@@ -53,7 +53,7 @@ object WebServer {
         } ~ ProductResource.route
 
 
-    val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
+    val bindingFuture = Http().bindAndHandle(route, "0.0.0.0", 8080)
 
     println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
     StdIn.readLine() // let it run until user presses return
